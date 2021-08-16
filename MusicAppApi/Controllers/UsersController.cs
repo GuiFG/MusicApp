16bafebd3145ccf5df8e757c9d883ce6f721120c
GuiFG <gui.2786@hotmail.com>
2021-08-16 20:08:05 -0300

    Module Test

--- MusicAppApi/Controllers/UsersController.cs
+++ MusicAppApi/Controllers/UsersController.cs
@@ -103,4 +103,5 @@ namespace MusicAppApi.Controllers
             }       
         }
     }
+
 }
---
+++ MusicAppTest/UserTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace PasswordValidatorTests
+{
+  public class UserTest
+  {
+    [Fact]
+    public void SaveUserTest()
+    {
+      //Arrange
+      //Act
+      //Assert
+    }
+  }
+}
